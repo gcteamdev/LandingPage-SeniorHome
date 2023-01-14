ad49414b14462a3bf3bf968d1ac2681ceafa7c56
GC <contact@gcteam.dev>
2023-01-13 16:45:57 -0800

    added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import banner from './components/banner';
 
 function App() {
   return (
     <div className="App">
-      
-      
+
+      <banner />
     </div>
   );
 }
---
+++ src/components/banner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./banner.css";
+
+function banner() {
+  return (
+    <div className="bannerPage">banner</div>
+  )
+}
+
+export default banner;
